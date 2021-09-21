1075112dd9ba974ece7ff0516876396666cddcad
Igor Soroka <igor@soroka.tech>
2021-09-21 14:12:41 +0300

    adding prettier

--- workshop/lib/workshop-stack.ts
+++ workshop/lib/workshop-stack.ts
@@ -1,6 +1,6 @@
-import { aws_apigateway as apigw, aws_lambda as lambda } from 'aws-cdk-lib'
-import { Stack } from "aws-cdk-lib";
-import { Construct } from "constructs";
+import { aws_apigateway as apigw, aws_lambda as lambda } from 'aws-cdk-lib';
+import { Stack } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
 export class WorkshopStack extends Stack {
   constructor(scope: Construct, id: string) {
@@ -8,13 +8,13 @@ export class WorkshopStack extends Stack {
 
     // The code that defines your stack goes here
     // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, "HelloHandler", {
+    const hello = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_10_X, // execution environment
-      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
-      handler: "hello.handler", // file is "hello", function is "handler"
+      code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" directory
+      handler: 'hello.handler', // file is "hello", function is "handler"
     });
     // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, "Endpoint", {
+    new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: hello,
     });
   }
