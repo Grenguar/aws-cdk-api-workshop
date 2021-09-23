dd38b6fd0a06d9cd8b7b7e60958f949d7563f965
Igor Soroka <igor@soroka.tech>
2021-09-23 11:19:04 +0300

    removing comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,6 @@
 const path = require('path');
 const entry = require('webpack-glob-entry');
 
-// const dir = 'src'
-// const entry = readdirSync(dir)
-//   .filter(item => /\.([tj])s$/.test(item))
-//   .filter(item => !/\.d\.([tj])s$/.test(item))
-//   .reduce((acc, fileName) => ({
-//     ...acc,
-//     [fileName.replace(/\.([tj])s$/, '')]: `./${dir}/${fileName}`
-//   }), {})
-
 module.exports = {
   entry: entry('./src/functions/*.ts'),
   mode: 'production',
