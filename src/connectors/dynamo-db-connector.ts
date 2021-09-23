116074ae5756842eccbe2641d7df44676fee3c94
Igor Soroka <igor@soroka.tech>
2021-09-23 10:01:06 +0300

    fixing naming for create

--- src/connectors/dynamo-db-connector.ts
+++ src/connectors/dynamo-db-connector.ts
@@ -1,9 +1,10 @@
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 import { Book } from '../models/book';
-const uuid = require('uuid');
+import uuid from 'uuid';
+
 const dynamo = new DocumentClient();
 
-export async function put(table: string, book: Book) {
+export async function create(table: string, book: Book) {
   const params = {
     TableName: table,
     Item: {
--- src/functions/create.ts
+++ src/functions/create.ts
@@ -1,10 +1,10 @@
 import { APIGatewayProxyEventV2, Callback, Context } from 'aws-lambda';
-import { put } from '../connectors/dynamo-db-connector';
+import { create } from '../connectors/dynamo-db-connector';
 
 export async function handler (event: APIGatewayProxyEventV2, context: Context, callback: Callback) {
   if (typeof event.body === 'string') {
     const bookItem = JSON.parse(event.body);
-    const createBook = await put(process.env.table as string, bookItem);
+    const createBook = await create(process.env.table as string, bookItem);
     const response = {
       statusCode: 200,
       body: JSON.stringify(createBook)
